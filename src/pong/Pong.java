d14ea012f3890a97017daaa689c726c010ddbfc7
Nacho <Nacho@192.168.1.127>
2019-10-30 11:56:35 +0000

    Controls: Player 1 (Left): W/S Player 2 (Right): UP/DOWN

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pong;
 
 import java.util.Random;
@@ -21,8 +16,11 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 /**
- *
- * @author Nacho
+ * @author Ignacio Sierra Clavijo
+ * 
+ * Controls:
+ * Player 1 (Left): W/S
+ * Player 2 (Right): UP/DOWN
  */
 public class Pong extends Application {
     
